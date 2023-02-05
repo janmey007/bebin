1e24dbb97256b30e5d39f22bdbe9e86c3d225aa5
Janmey Ganesh Patil <117360870+janmey007@users.noreply.github.com>
2023-02-06 00:11:56 +0530

    invention.java

--- src/invention.java
+++ src/invention.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class soham {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(system.in)
+        Scanner sc = new Scanner(System.in)
 
 
         String a = sc.nextLine();
